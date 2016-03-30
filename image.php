f2e8bf21aa1d4cacf13b4e1559bf5e0b4002d7d9
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2016-03-30 20:21:56 +0600

    Delete image.php

--- image.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: image/*');
-$name = filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING);
-$url = "https://raw.githubusercontent.com/MuntashirAkon/Mac-OS-Installation-Helper/master/";
-$url .= $name;
-header('Content-Disposition: attachment; filename="{$name}"');
-readfile($url);
